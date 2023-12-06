700bf982b39408b50857a14f8358873ed53b7b04
Chagas Filho <106563976+chagasfilho007@users.noreply.github.com>
2023-12-06 16:14:12 -0300

    Update cadastro.php

--- cadastro.php
+++ cadastro.php
@@ -1,2 +1,6 @@
-Os dados foram enviados!
-Aprenda PHP para saber o que fazer com eles.
+<html>
+    <body>
+        Os dados foram enviados!
+        Aprenda PHP para saber o que fazer com eles.
+    </body>
+</html>
